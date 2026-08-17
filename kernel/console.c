881dfd4040dc868061ff4bd3cd26553aed3201e3
mael29430 <maeldescamps.29@gmail.com>
2026-08-17 23:18:12 +0200

    Create console.c with initialization and write functions
    Add initial console implementation for MyOS kernel.

---
+++ kernel/console.c
@@ -0,0 +1,23 @@
+/*
+ * MyOS Console
+ *
+ * Premier système d'affichage
+ * du kernel.
+ */
+
+void console_init(void)
+{
+    /*
+     * Initialisation future
+     * de la console MyOS.
+     */
+}
+
+
+void console_write(const char* text)
+{
+    /*
+     * Affichage futur des messages
+     * du système.
+     */
+}
