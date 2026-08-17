00e1daa764a44d5975cfa3cae737b5c2e57f92d6
mael29430 <maeldescamps.29@gmail.com>
2026-08-17 23:36:45 +0200

    Implement initial task manager structure
    Added initial structure for task manager with placeholder functions for initialization and task creation.

---
+++ system/tasks/task.c
@@ -0,0 +1,28 @@
+/*
+ * MyOS Task Manager
+ *
+ * Gestion future des applications
+ * et des processus.
+ */
+
+
+void task_init(void)
+{
+
+    /*
+     * Initialisation future
+     * du gestionnaire de tâches.
+     */
+
+}
+
+
+void task_create(void)
+{
+
+    /*
+     * Création future
+     * d'une nouvelle tâche.
+     */
+
+}
