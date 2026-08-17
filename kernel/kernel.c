23e13f6d830d687c27df47e395ece812f8d179bf
mael29430 <maeldescamps.29@gmail.com>
2026-08-17 23:19:39 +0200

    Initialize console in kernel_main function
    Added console initialization to the kernel entry point.

--- kernel/kernel.c
+++ kernel/kernel.c
@@ -4,18 +4,39 @@
  * Premier noyau expérimental ARM64.
  */
 
+
+/* Fonction de la console */
+
+void console_init(void);
+
+
+/*
+ * Point d'entrée du kernel
+ */
+
 void kernel_main(void)
 {
+
     /*
-     * Point d'entrée du kernel.
-     *
-     * Le code sera développé progressivement.
+     * Initialisation
+     * des services système
+     */
+
+    console_init();
+
+
+    /*
+     * Boucle principale
+     * du kernel
      */
 
     while (1)
     {
+
         /*
          * Le kernel reste actif.
          */
+
     }
+
 }
