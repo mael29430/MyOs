4c4774ff2cb8f0357e1ed8e72ac2163b922feb66
mael29430 <maeldescamps.29@gmail.com>
2026-08-17 22:34:09 +0200

    Create kernel.c

---
+++ kernel/kernel.c
@@ -0,0 +1,14 @@
+/*
+ * MyOS Kernel
+ * Premier prototype du noyau
+ */
+
+void kernel_main(void)
+{
+    /*
+     * Point d'entrée du kernel MyOS.
+     *
+     * Pour le moment, nous ne faisons
+     * qu'indiquer que le kernel a démarré.
+     */
+}
