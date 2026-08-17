bc808abdf34b4301fc206e3ee2e794060593c521
mael29430 <maeldescamps.29@gmail.com>
2026-08-17 23:27:31 +0200

    Add initial memory manager implementation

---
+++ system/memory/memory.c
@@ -0,0 +1,16 @@
+/*
+ * MyOS Memory Manager
+ *
+ * Gestion future de la mémoire système.
+ */
+
+
+void memory_init(void)
+{
+
+    /*
+     * Initialisation future
+     * du gestionnaire mémoire.
+     */
+
+}
