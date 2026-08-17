f3824011a4dd1cc1658c26010e024c63f388698e
mael29430 <maeldescamps.29@gmail.com>
2026-08-17 23:45:53 +0200

    Create device.c

---
+++ system/devices/device.c
@@ -0,0 +1,28 @@
+/*
+ * MyOS Device Manager
+ *
+ * Gestion des périphériques
+ * du système.
+ */
+
+
+void device_init(void)
+{
+
+    /*
+     * Initialisation future
+     * des périphériques.
+     */
+
+}
+
+
+void device_register(void)
+{
+
+    /*
+     * Ajout futur d'un
+     * nouveau périphérique.
+     */
+
+}
