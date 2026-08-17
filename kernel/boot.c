5950a432651e666710ef47c4fc8e79512af29b5b
mael29430 <maeldescamps.29@gmail.com>
2026-08-17 22:37:30 +0200

    Add boot_main function for MyOS boot process

---
+++ kernel/boot.c
@@ -0,0 +1,17 @@
+/*
+ * MyOS Boot
+ *
+ * Premier programme de démarrage
+ * du projet MyOS.
+ */
+
+void boot_main(void)
+{
+    /*
+     * Initialisation du système.
+     *
+     * Plus tard, cette fonction
+     * préparera le matériel avant
+     * de lancer le kernel.
+     */
+}
