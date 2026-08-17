bea31cb6baebee4daf60752b88bfbc467099cf12
mael29430 <maeldescamps.29@gmail.com>
2026-08-17 23:30:20 +0200

    Create storage.c

---
+++ system/storage/storage.c
@@ -0,0 +1,20 @@
+/*
+ * MyOS Storage Manager
+ *
+ * Gestion future du stockage
+ * et du système de fichiers.
+ */
+
+
+void storage_init(void)
+{
+
+    /*
+     * Initialisation future :
+     *
+     * - mémoire de stockage
+     * - fichiers
+     * - données système
+     */
+
+}
