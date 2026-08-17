8684909588ac08e4d7b341f55077cbb840be95d4
mael29430 <maeldescamps.29@gmail.com>
2026-08-17 23:41:55 +0200

    Implement syscall_init and syscall_execute functions
    Added initial system call functions for MyOS.

---
+++ system/syscall/syscall.c
@@ -0,0 +1,28 @@
+/*
+ * MyOS System Calls
+ *
+ * Interface entre les applications
+ * et le kernel.
+ */
+
+
+void syscall_init(void)
+{
+
+    /*
+     * Initialisation future
+     * des appels système.
+     */
+
+}
+
+
+void syscall_execute(void)
+{
+
+    /*
+     * Exécution future
+     * des demandes des applications.
+     */
+
+}
