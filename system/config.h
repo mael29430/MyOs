f2765d59e69b72b7ff329d81d2b4fb598e2edce4
mael29430 <maeldescamps.29@gmail.com>
2026-08-18 13:37:58 +0200

    Update MyOS configuration with memory and version info
    Added memory configuration and kernel version definitions.

--- system/config.h
+++ system/config.h
@@ -1,14 +1,28 @@
 #ifndef MYOS_CONFIG_H
 #define MYOS_CONFIG_H
 
+/*
+ * MyOS configuration
+ */
 
-#define MYOS_NAME "MyOS"
+/*
+ * Mémoire QEMU virt.
+ *
+ * Pour notre première plateforme AArch64,
+ * QEMU fournit une zone RAM suffisante
+ * pour le kernel et ses structures.
+ */
 
-#define MYOS_VERSION "0.1"
+#define MYOS_MEMORY_START 0x41000000UL
+#define MYOS_MEMORY_END   0x42000000UL
 
-#define MYOS_ARCH "ARM64"
+#define MYOS_PAGE_SIZE 4096UL
 
-#define MYOS_DEVELOPER "Mael"
+#define MYOS_MAX_TASKS 16
 
+#define MYOS_MAX_FILES 32
+
+#define MYOS_KERNEL_VERSION_MAJOR 0
+#define MYOS_KERNEL_VERSION_MINOR 1
 
 #endif
