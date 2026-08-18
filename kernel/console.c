c286f368ca6996d7bec1cdd1888a87b7d56112e1
mael29430 <maeldescamps.29@gmail.com>
2026-08-18 11:16:16 +0200

    Refactor console.c for QEMU UART support
    Updated console implementation for QEMU virtual UART.

--- kernel/console.c
+++ kernel/console.c
@@ -1,21 +1,26 @@
 /*
  * MyOS Console
  *
- * Premier système d'affichage
- * du kernel.
+ * Console série pour QEMU virt.
  */
 
-#define UART0_BASE 0x09000000UL
+#define UART_BASE 0x09000000UL
 
-volatile unsigned int* const UART0 =
-    (volatile unsigned int*)UART0_BASE;
+#define UARTDR 0x00
+#define UARTFR 0x18
+
+#define UARTFR_TXFF (1 << 5)
+
+static volatile unsigned int* const uart =
+    (volatile unsigned int*)UART_BASE;
 
 
 void console_init(void)
 {
     /*
-     * Initialisation future
-     * de la console MyOS.
+     * QEMU virt fournit déjà l'UART.
+     * Il n'y a pas besoin d'une initialisation
+     * matérielle complexe pour notre premier test.
      */
 }
 
@@ -24,7 +29,21 @@ void console_write(const char* text)
 {
     while (*text)
     {
-        *UART0 = *text;
+        /*
+         * Attendre que le FIFO de transmission
+         * ne soit pas plein.
+         */
+
+        while (uart[UARTFR / 4] & UARTFR_TXFF)
+        {
+        }
+
+        /*
+         * Envoyer le caractère.
+         */
+
+        uart[UARTDR / 4] = (unsigned int)*text;
+
         text++;
     }
 }
