3b29bb5f193354ff83f107b6672ed1c49b913886
mael29430 <maeldescamps.29@gmail.com>
2026-08-18 12:46:28 +0200

    Update console implementation for QEMU support
    Refactor console functions for QEMU compatibility and improve UART handling.

--- kernel/console.c
+++ kernel/console.c
@@ -1,28 +1,52 @@
 /*
  * MyOS Console
  *
- * Première console texte de MyOS.
+ * Console série minimale pour QEMU.
  */
 
 #define UART_BASE 0x09000000UL
 
-static volatile unsigned int *const UART =
-    (volatile unsigned int *)UART_BASE;
+#define UART_DR (*(volatile unsigned int *)(UART_BASE + 0x00))
 
+
+/*
+ * Initialise la console.
+ *
+ * Pour QEMU "virt", l'UART est déjà
+ * suffisamment configuré pour notre
+ * premier test.
+ */
 
 void console_init(void)
 {
-    /*
-     * Initialisation minimale de la console.
-     */
 }
 
+
+/*
+ * Écrit un caractère sur la console.
+ */
+
+static void console_putc(char c)
+{
+    UART_DR = (unsigned int)c;
+}
+
+
+/*
+ * Écrit une chaîne de caractères.
+ */
 
 void console_write(const char *text)
 {
     while (*text)
     {
-        *UART = (unsigned int)*text;
+        if (*text == '\n')
+        {
+            console_putc('\r');
+        }
+
+        console_putc(*text);
+
         text++;
     }
 }
